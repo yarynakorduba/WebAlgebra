356c7aa4ef4784d981a428659e631739f872b72d
yarynakorduba <korduba_y@ucu.edu.ua>
2016-11-08 18:15:38 +0200

    fixed end-matrix web representation

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, render_template, request
-
+from inverse_via_lu import Matrix
 app = Flask(__name__)
 
-def tranpose(s):
+def to_matrix(s):
     matrix = []
     lines = s.split('\n')
     for l in lines:
-        matrix.append(l.split())
+        matrix.append(list(map(float, l.split())))
 
-    m = len(matrix)
-    n = len(matrix[0])
+    return matrix
 
+def matrix_to_sring(m):
     new_matrix = '$\\left(\\begin{matrix}\n'
-
-
-    for j in range(n):
-        line = ''
-        for i in range(m):
-            line += str(matrix[i][j])
-            if i != m - 1:
-                line += '&'
+    for el in range(len(m)):
+        answer = ''
+        for ele in range(len(m[el])):
+            if m[el][ele] == int(m[el][ele]):
+                answer += str(m[el][ele])[:-2]
             else:
-                line += '\\\\\n'
-        new_matrix += line
+                answer += str(round(m[el][ele], 3))
 
+            if ele != (len(m[el])-1):
+                answer += '&'
+
+            else:
+                answer += '\\\\\n'
+
+        new_matrix += answer
     new_matrix += '\\end{matrix}\\right)$\n'
-
     return new_matrix
 
 
@@ -34,10 +36,13 @@ def tranpose(s):
 def get_index():
     if request.method == "POST":
         matrix = request.form['matrix']
-        new_matrix = tranpose(matrix)
+        new_matrix = Matrix(to_matrix(matrix))
+        new_matrix = new_matrix.matrix_inverse()
+     #   new_matrix = '$\\left(\\begin{matrix}\n'
+        new_matrix = matrix_to_sring(new_matrix)
         return render_template('index.html', matrix=matrix, new_matrix=new_matrix)
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
